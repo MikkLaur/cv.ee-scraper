2fa3fd88ff2b7a2ce635eb2f964acc677019d1f2
Mikk <32076851+MikkLaur@users.noreply.github.com>
2018-02-23 16:45:12 +0200

    Update utility.py

--- utility.py
+++ utility.py
@@ -1,7 +1,7 @@
 ##  Convert database data with old end_date format 
 ## to new end_date format
 # Old end_date format: dd.mm.YYYY
-# New end_date format: YYYY.mm.dd
+# New end_date format: YYYY-mm-dd
 #
 #  this makes the end_date alphabetically sortable with
 # sql query
